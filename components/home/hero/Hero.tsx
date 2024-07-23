2a2d4296c33c44a2fe6db737169533a388eaae90
Zahra Abhari <Zahra.Abhari@shab.local>
2024-07-23 18:31:38 +0330

    fix search suggestion

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -2,22 +2,35 @@ import SearchIcon from '@/components/icons/SearchIcon'
 import Button from '@/components/ui/button/Button'
 import { ApiRoutes } from '@/constants/routes'
 import UseFetch from '@/hooks/useFetch'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 import { FC, useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 type careerItemType = { id: number; title: string; category: string }
 type careerType = { data: careerItemType[] }
 const Hero: FC = () => {
-  const { request, response } = UseFetch()
   const [career, setCareer] = useState('')
+  const [response, setResponse] = useState()
+  const router = useRouter()
   const suggestions: careerItemType[] =
     career.length > 0 && response
       ? (response as careerType)?.data?.filter((item) => item.title.toLocaleLowerCase().includes(career))
       : []
   useEffect(() => {
-    request({
-      url: ApiRoutes.CAREERS,
-      method: 'get',
-    })
+    axios
+      .get(ApiRoutes.BASE + ApiRoutes.CAREERS)
+      .then((res) => {
+        setResponse(res?.data)
+      })
+      .catch((e) => {
+        // toast.error(e?.response?.data?.errors[0])
+      })
+      .finally(() => {})
   }, [])
+  const careerHandler = (value: number) => {
+    sessionStorage.setItem('preferences', JSON.stringify({ career: value }))
+    router.push('/preferences/budget')
+  }
   return (
     <div className="lg:max-w-[80%] xs:max-w-full flex justify-center items-center text-center">
       <div className="flex flex-col justify-center items-center 2xl:max-w-[80%] xs:max-w-[90%] mb-12">
@@ -26,7 +39,7 @@ const Hero: FC = () => {
           Envision Your Success with AI-Powered Paths toward Your Future Success
         </span>
 
-        <form className=" mx-auto w-full mt-[56px] ">
+        <form className=" mx-auto w-full mt-[56px] relative">
           <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
             Search
           </label>
@@ -53,10 +66,15 @@ const Hero: FC = () => {
             </Button>
           </div>
           {suggestions?.length > 0 && (
-            <div className="max-h-60 overflow-auto shadow-[0_4px_18px_rgba(0,0,0,0.1)] rounded-[16px]  transform translate-y-[10px] z-10 border-t border-gray-600 w-full bg-white">
+            <div className="max-h-60 overflow-auto shadow-[0_4px_18px_rgba(0,0,0,0.1)] rounded-[16px] absolute  transform translate-y-[10px] z-10 border-t border-gray-600 w-full bg-white">
               <ul className="py-2  text-sm text-gray-700 divide-y divide-gray-100">
                 {suggestions.map((item, index) => (
-                  <li key={'SUGGESTION_ITEM_' + index} className=" py-2 px-4" onClick={() => {}}>
+                  <li
+                    key={'SUGGESTION_ITEM_' + index}
+                    className=" py-2 px-4"
+                    onClick={() => {
+                      careerHandler(item.id)
+                    }}>
                     {item.title}
                   </li>
                 ))}
