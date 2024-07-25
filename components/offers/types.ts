f448aa943e0cdf898a03695416c2fcf1bf90f8de
Zahra Abhari <Zahra.Abhari@shab.local>
2024-07-25 16:19:21 +0330

    api call in offer page

---
+++ components/offers/types.ts
@@ -0,0 +1,25 @@
+export interface dataItemType {
+  URL: string
+  authors: string[]
+  description: string
+  id: number
+  level: number
+  picture: string
+  price: number
+  publisher: string
+  skills: string[]
+  title: string
+  Language: string
+  number_of_pages: number
+  channel: string
+  duration: number
+  publication: string
+}
+export interface dataType {
+  articles: dataItemType[]
+  books: dataItemType[]
+  'online courses': dataItemType[]
+  'youtube videos': dataItemType[]
+}
+
+export type DataTypeKey = keyof dataType
